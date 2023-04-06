f99780bd37467788861a9a168b4884e2acf8614a
Masivo <m4ssico@gmail.com>
2023-04-06 07:55:30 -0400

    websocket added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Minecraft-Tracker';
+
+  generalData = {};
+  serversData = {};
+  graphData!: any;
+
+  ngOnInit() {
+    const subject = webSocket('wss://masivo.cc');
+
+    subject.subscribe({
+      next: msg => {
+        const data = JSON.parse(JSON.stringify(msg));
+        if(data.message == 'init') {
+          this.generalData = data;
+          subject.next('requestHistoryGraph');
+        } else if(data.message == 'updateServers') {
+          this.serversData = data;
+        } else if(data.message == 'historyGraph') {
+          this.graphData = data;
+        }
+        
+      }, error: err => {
+        console.log(err);
+      }
+    });
+  }
 }
