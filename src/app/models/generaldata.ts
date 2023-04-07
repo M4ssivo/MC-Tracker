6b4aee7d396a0d239b69dad480da6b85748d8002
Masivo <m4ssico@gmail.com>
2023-04-06 20:03:08 -0400

    GeneralData model

---
+++ src/app/models/generaldata.ts
@@ -0,0 +1,24 @@
+import { ServerData } from "./serverdata";
+import { ServerInfo } from "./serverinfo";
+
+export interface GeneralData {
+    config: {
+        graphDurationLabel: string,
+        graphMaxLength: number,
+        isGraphVisible: boolean,
+        minecraftVersions: {
+            PC: []
+        },
+        serverGraphMaxLength: number,
+        servers: ServerInfo[],
+    }
+    message: string,
+    mojangServices: {
+        API: string,
+        Auth: string,
+        Sessions: string,
+        Skins: string
+    },
+    servers: ServerData[],
+    timestampPoints: []
+}
