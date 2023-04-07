fde427c4827921ce196ed2798ae9d6dd57eff501
Masivo <m4ssico@gmail.com>
2023-04-06 20:02:31 -0400

    websocket service created

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  public subject!: WebSocketSubject<any>;
+
+  constructor() { }
+
+  connect() {
+    if (!this.subject) {
+      this.subject = webSocket('wss://masivo.cc');
+    }
+    return this.subject;
+  }
+}
